377aab5d882a10f36e6c64b6b15fe3446824db6c
reeha <56428237+syedareehaquasar@users.noreply.github.com>
2020-03-24 15:30:26 +0530

    create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+def isvowel(letter):
+    if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u' or letter == 'A' or letter == 'E' or letter == 'I' or letter == 'O' or letter == 'U':
+        return True
+    else:
+        return False
+
+print(isvowel('e'))

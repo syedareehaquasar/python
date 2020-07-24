fe4439eb2b21a85f12420784335e99147ab1ad8d
syedareehaquasar <reeha.saba@gmail.com>
2020-07-24 23:36:05 +0530

    24-07-2020 cass problem

---
+++ randommatch.py
@@ -0,0 +1,16 @@
+import random
+
+toChooseFrom = "abcdefghijklmnopqrstuvwxyz "
+
+def randomStrs(x):
+    # return "".join([random.choice(toChooseFrom) for i in range(x)])
+    return random.choices(toChooseFrom, k = 28)
+
+
+s = input()
+_ = 1
+while randomStrs(28) != s:
+    if _ % 1000 == 0:
+        print(_ , randomStrs(28))
+    _ += 1
+print(_, s, "best")

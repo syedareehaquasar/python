7d833a8ffe7ea67b2503b68fa1924044a4e6234d
syedareehaquasar <reeha.saba@gmail.com>
2020-09-29 19:51:41 +0530

    covered a cornered case

--- sumPairs.py
+++ sumPairs.py
@@ -1,13 +1,23 @@
 def sumPairs(arr : [int], givenSum : int) -> [(int, int)]:
-    s = set()
+    s = []
+    t = []
     pairs = []
     for element in arr:
         x = givenSum - element
         if x in s:
             pairs.append((element, x))
-        s.add(element)
+            if arr.count(x) > 2 and arr.count(element) > 2:
+                t.append(element)
+        else:
+            s.append(element)
+    if t:
+        for element in t:
+            x = givenSum - element
+            if x in arr:
+                pairs.append((element, x))
     return pairs
 
-print(sumPairs([1, 5, 7, -1], 6))
-print(sumPairs([1, 5, 7, -1, 5], 6))
+print(sumPairs([1, 5, 7, -1], 6)) #[(5, 1), (-1, 7)]
+print(sumPairs([1, 5, 7, -1, 5], 6)) #[(5, 1), (-1, 7), (5, 1)]
 print(sumPairs([1, 1, 1, 1], 2))
+

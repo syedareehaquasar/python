6aa6fc57caea690c0f9567bd3cfcb96e7c5901fc
syedareehaquasar <reeha.saba@gmail.com>
2020-09-29 20:16:06 +0530

    removed a logic confussion

--- sumPairs.py
+++ sumPairs.py
@@ -1,23 +1,25 @@
-def sumPairs(arr : [int], givenSum : int) -> [(int, int)]:
+def sumPairs(arr : [int], givenSum : int) -> []:
     s = []
-    t = []
     pairs = []
     for element in arr:
         x = givenSum - element
         if x in s:
             pairs.append((element, x))
-            if arr.count(x) > 2 and arr.count(element) > 2:
-                t.append(element)
-        else:
-            s.append(element)
-    if t:
-        for element in t:
-            x = givenSum - element
-            if x in arr:
+            if min(arr.count(x), arr.count(element)) > 1:
                 pairs.append((element, x))
-    return pairs
+        s.append(element)
+    return pairs if pairs else "There are no such pairs"
 
 print(sumPairs([1, 5, 7, -1], 6)) #[(5, 1), (-1, 7)]
 print(sumPairs([1, 5, 7, -1, 5], 6)) #[(5, 1), (-1, 7), (5, 1)]
 print(sumPairs([1, 1, 1, 1], 2))
+print(sumPairs([1, 1, 1, 1], 6))
 
+
+# def addingPairs(arr, givenSum):
+#     pairs = []
+#     for element in arr:
+#         x = givenSum - element
+#         if x in s:
+#             pairs.append((element, x))
+#     return pairs
